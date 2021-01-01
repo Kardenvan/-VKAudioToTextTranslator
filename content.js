b72ed8f4da33ca05b504753059f05e364e2245c5
Kardenvan <kmlichking1234@mail.ru>
2021-01-01 18:40:31 +0300

    VKATTT-000002 / Сбор ссылок и получение контейнеров для последующей вставки текста

--- content.js
+++ content.js
@@ -2,13 +2,16 @@ chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
         if( request.message === "clicked_browser_action" ) {
             let messages = $('div[data-mp3^="https://psv4.userapi.com"]');
-            let links = [];
+
+            let links = {};
 
             console.log(messages);
 
             for (let message of messages) {
                 let link = $(message).attr('data-mp3');
-                links.push(link);
+                let containerId = $(message).parent().attr('id');
+
+                links[containerId] = link;
             }
 
             console.log(links);
